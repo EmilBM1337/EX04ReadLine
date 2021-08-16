b5d2a5ed3a24dfd8c36ad5187b935a1e89c6e782
emil41m5 <emil41m5@CV-BB-5340>
2021-08-16 10:26:59 +0200

    Add project files.

---
+++ EX04ReadLine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EX04ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv dit fornavn.");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Skriv dit efternavn.");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Velkommen {firstName} {lastName}");
+            Console.WriteLine("Skriv din alder.");
+            int age = int.Parse(Console.ReadLine());
+        }
+    }
+}
