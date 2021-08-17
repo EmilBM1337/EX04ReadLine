0cd25413dc91740119a4c3d1ca117e350cdcd502
emil41m5 <emil41m5@CV-BB-5340>
2021-08-17 11:41:37 +0200

    .

--- EX04ReadLine/Program.cs
+++ EX04ReadLine/Program.cs
@@ -6,13 +6,23 @@ namespace EX04ReadLine
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 200;
+
             Console.WriteLine("Skriv dit fornavn.");
             string firstName = Console.ReadLine();
+            Console.WriteLine("");
             Console.WriteLine("Skriv dit efternavn.");
             string lastName = Console.ReadLine();
+            Console.WriteLine("");
             Console.WriteLine($"Velkommen {firstName} {lastName}");
+            Console.WriteLine("");
             Console.WriteLine("Skriv din alder.");
             int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine($"Din alder er {age}.");
         }
     }
 }
